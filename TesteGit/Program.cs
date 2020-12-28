f2d0fe0462a7bc42459868c55944a3cc13e5a2e4
Leonardo Avelino <leoave46@gmail.com>
2020-12-28 18:34:02 -0300

    treino inicial, github

---
+++ TesteGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O numero digitado foi " + n);
+
+        }
+    }
+}
